6836468b8e22768bbf1e6920269e28a469c0e9ad
rishabh-star <risingmerta@gmail.com>
2026-01-20 19:10:07 +0530

    first commit

--- src/lib/mongoClient.js
+++ src/lib/mongoClient.js
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const MONGODB_URI = "mongodb://admin:imperial_king2030@127.0.0.1:27017/?authSource=admin";
+const MONGODB_URI = "mongodb://root:imperial_king2030@127.0.0.1:27017/?authSource=admin";
 const MONGODB_DB = "mydatabase";
 
 if (!MONGODB_URI) throw new Error("Please define MONGODB_URI in .env");
